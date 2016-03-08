6592fb8e2d38a136b55e69ee94cd61a8a1ecfade
Dorian Dressler <Dorian Dressler>
2016-03-08 18:10:11 +0000

    minor code changes in frame

--- src/main/java/net/spacejunkjim/miniproject3/WeatherFrame.java
+++ src/main/java/net/spacejunkjim/miniproject3/WeatherFrame.java
@@ -138,10 +138,8 @@ public class WeatherFrame extends javax.swing.JFrame {
      */
     public static void main(String args[]) {
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new WeatherFrame().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new WeatherFrame().setVisible(true);
         });
     }
 
