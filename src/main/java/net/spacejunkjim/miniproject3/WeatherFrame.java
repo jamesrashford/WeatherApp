3b5c4ea125165d0b3aa2589c3946192b7913e7d3
Dorian Dressler <Dorian Dressler>
2016-03-08 18:21:42 +0000

    Removed Lambda Expression for sake of preserving world peace.

--- src/main/java/net/spacejunkjim/miniproject3/WeatherFrame.java
+++ src/main/java/net/spacejunkjim/miniproject3/WeatherFrame.java
@@ -138,8 +138,11 @@ public class WeatherFrame extends javax.swing.JFrame {
      */
     public static void main(String args[]) {
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> {
-            new WeatherFrame().setVisible(true);
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new WeatherFrame().setVisible(true);
+            }
         });
     }
 
