311ff80a197f1b802bf1939230e56476c22c983b
James <eeu48c@bangor.ac.uk>
2016-03-09 15:59:22 +0000

    Added more weather conditions

--- src/main/java/net/spacejunkjim/miniproject3/WeatherConditions.java
+++ src/main/java/net/spacejunkjim/miniproject3/WeatherConditions.java
@@ -10,49 +10,39 @@ package net.spacejunkjim.miniproject3;
  * @author jamesashford
  */
 public class WeatherConditions {
-
-    /*
     public static final String WEATHER_SUNNY_DAY = "Sunny Day";
     public static final String WEATHER_PARTLY_CLOUDY = "Partly cloudy (night)";
     public static final String WEATHER_SUNNY_INTERVALS = "Sunny Intervals";
     public static final String WEATHER_MEDIUM_COLOUD = "White Medium Level Cloud";
-    8:   black low level cloud
-    9:   light rain shower (night)
-    10: light rain shower (day)
-    11: drizzle
-    12: light rain day or night
-    13: heavy rain shower (night)
-    14: heavy rain shower (day)
-    15: heavy rain
-    16: sleet shower (night)
-    17: sleet shower (day)
-    18: cloudy with sleet
-    19: hail shower (night)
-    20: hail shower (day)
-    21: cloudy with hail
-    22: light snow shower (night)
-    23: light snow shower (day)
-    24: cloudy with light snow
-    25: heavy snow shower (night)
-    26: heavy snow shower (day)
-    27: cloudy with heavy snow
-    28: thundery shower (night)
-    29: thundery shower (day)
-    30: thunderstorms
-    The remainder have been represented variously by plain text or images. They are :
-    0: clear sky (night)
-    4: sandstorm
-    5: mist day / night
-    6: fog day / night
-    31: tropical storm
-    32: hazy
-     */
-    public static final String WEATHER_SUNNY = "Sunny";
-    public static final String WEATHER_CLOUDY = "Cloudy";
-    public static final String WEATHER_LIGHT_CLOUDS = "Light Clouds";
-    public static final String WEATHER_WINDY = "Windy";
-    public static final String WEATHER_HEAVY_RAIN = "Heavy Rain";
-    public static final String WEATHER_RAIN_SHOWERS = "Rain Showers";
-    public static final String WEATHER_SNOW = "Snow";
-    public static final String WEATHER_LIGHTNING = "Lightning";
+    public static final String WEATHER_BLACK_LOW_LEVEL_CLOUD = "black low level cloud";
+    public static final String WEATHER_LIGHT_RAIN_SHOWER_NIGHT = "light rain shower (night)";
+    public static final String WEATHER_LIGHT_RAIN_SHOWER_DAY = "light rain shower (day)";
+    public static final String WEATHER_DRIZZLE = "drizzle";
+    public static final String WEATHER_LIGHT_RAIN_DAY_NIGHT = "light rain day or night";
+    public static final String WEATHER_HEAVY_RAIN_SHOWER_NIGHT = "heavy rain shower (night)";
+    public static final String WEATHER_HEAVY_RAIN_SHOWER_DAY = "heavy rain shower (day)";
+    public static final String WEATHER_HEAVY_RAIN = "heavy rain";
+    public static final String WEATHER_SLEET_SHOWER_NIGHT = "sleet shower (night)";
+    public static final String WEATHER_SLEET_SHOWER_DAY = "sleet shower (day)";
+    public static final String WEATHER_CLOUDY_WITH_SLEET = "cloudy with sleet";
+    public static final String WEATHER_HAIL_SHOWER_NIGHT = "hail shower (night)";
+    public static final String WEATHER_HAIL_SHOWER_DAY = "hail shower (day)";
+    public static final String WEATHER_CLOUDY_HAIL = "cloudy with hail";
+    public static final String WEATHER_LIGHT_SNOW_SHOWER_NIGHT = "light snow shower (night)";
+    public static final String WEATHER_LIGHT_SNOW_SHOWER_DAY = "light snow shower (day)";
+    public static final String WEATHER_CLOUDY_LIGHT_SNOW = "cloudy with light snow";
+    public static final String WEATHER_HEAVY_SHOW_SHOWER_NIGHT = "heavy snow shower (night)";
+    public static final String WEATHER_HEAVY_SHOW_SHOWER_DAY = "heavy snow shower (day)";
+    public static final String WEATHER_CLOUDY_WITH_HEAVY_SNOW = "cloudy with heavy snow";
+    public static final String WEATHER_THUNDERY_SHOWER_NIGHT = "thundery shower (night)";
+    public static final String WEATHER_THUNDERY_SHOWER_DAY = "thundery shower (day)";
+    public static final String WEATHER_THUNDERSTORMS = "thunderstorms";
+    public static final String WEATHER_CLEAR_SKY_NIGHT = "clear sky (night)";
+    public static final String WEATHER_SANDSTORM = "sandstorm";
+    public static final String WEATHER_MIST_DAY = "mist day";
+    public static final String WEATHER_MIST_NIGHT = "mist night";
+    public static final String WEATHER_FOG_DAY = "fog day";
+    public static final String WEATHER_FOG_NIGHT = "fog night";
+    public static final String WEATHER_TROPICAL_STORM = "tropical storm";
+    public static final String WEATHER_HAZY = "hazy";
 }
