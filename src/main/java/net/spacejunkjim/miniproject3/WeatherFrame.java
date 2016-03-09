053d07fb4f012e1262663b4413d43f76a8480cce
Dorian Dressler <Dorian Dressler>
2016-03-09 16:51:15 +0000

    Added more icon support

--- src/main/java/net/spacejunkjim/miniproject3/WeatherFrame.java
+++ src/main/java/net/spacejunkjim/miniproject3/WeatherFrame.java
@@ -50,7 +50,6 @@ public class WeatherFrame extends javax.swing.JFrame {
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("BBC Weather Observations");
-        setResizable(false);
 
         appTitle.setFont(new Font("Lucida Grande", 0, 18)); // NOI18N
         appTitle.setText("BBC Weather Observations");
@@ -131,6 +130,7 @@ public class WeatherFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_appURLFieldActionPerformed
 
     private void appForecastButtonMouseClicked(MouseEvent evt) {//GEN-FIRST:event_appForecastButtonMouseClicked
+        
         // Clear the output
         jTextArea1.removeAll();
         jTextArea1.setText("");
@@ -142,21 +142,9 @@ public class WeatherFrame extends javax.swing.JFrame {
         } else {
             // Get a WeatherParser with URL
             WeatherParser parser = new WeatherParser(url);
-        
-            // Add output to display
-            String output = parser.getOutput();
-            jTextArea1.append(output);
-            jTextArea1.append("\n" + WeatherAppUtils.getWeatherCondition(output));
-            
-            
-            if (output.contains("null")) {
-                appWeatherIcon.setIcon(new ImageIcon(getClass().getResource("/simple_weather_icon_02.png")));
-            } else {
-                
-            }
-            //System.out.println("Test String: " + test);
-            
-        
+            String weatherData = parser.getOutput();
+            jTextArea1.append(weatherData);
+            setWeatherIcon(weatherData);
             // Clear URL field
             appURLField.setText("");
         }
@@ -179,6 +167,120 @@ public class WeatherFrame extends javax.swing.JFrame {
         });
     }
 
+    /**
+     * Method assigns icon to reflect the current weather.
+     */
+    private void setWeatherIcon(String comparator){
+        String fileLocation = null;
+        //comparator = "Partly cloudy (night)";
+        
+        if(comparator.contains("null")){ //test case
+            fileLocation = "/simple_weather_icon_02.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_SWUNNY_DAY)){
+            fileLocation = "/simple_weather_icon_01.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_PARTLY_CLOUDY)){
+            fileLocation = "/simple_weather_icon_07.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_SUNNY_INTERVALS)){
+            fileLocation = "/simple_weather_icon_03.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_MEDIUM_CLOUD)){
+            fileLocation = "/simple_weather_icon_06.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_BLACK_LOW_LEVEL_CLOUD)){
+            fileLocation = "/simple_weather_icon_06.png"; // finished
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_LIGHT_RAIN_SHOWER_NIGHT)){
+            fileLocation = "/simple_weather_icon_31.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_LIGHT_RAIN_SHOWER_DAY)){
+            fileLocation = "/simple_weather_icon_11.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_DRIZZLE)){
+            fileLocation = "/simple_weather_icon_21.png"; // here
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_LIGHT_RAIN_DAY_NIGHT)){
+            fileLocation = "/simple_weather_icon_21.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_HEAVY_RAIN_SHOWER_NIGHT)){
+            fileLocation = "/simple_weather_icon_32.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_HEAVY_RAIN_SHOWER_DAY)){
+            fileLocation = "/simple_weather_icon_33.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_HEAVY_RAIN)){
+            fileLocation = "/simple_weather_icon_23.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_HEAVY_RAIN_SHOWER_NIGHT)){
+            fileLocation = "/simple_weather_icon_33.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_SLEET_SHOWER_DAY)){
+            fileLocation = "/simple_weather_icon_16.png";
+        } 
+        if(comparator.contains(WeatherConditions.WEATHER_CLOUDY_WITH_SLEET)){
+            fileLocation = "/simple_weather_icon_26.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_HAIL_SHOWER_NIGHT)){
+            fileLocation = "/simple_weather_icon_38.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_HAIL_SHOWER_DAY)){
+            fileLocation = "/simple_weather_icon_19.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_CLOUDY_HAIL)){
+            fileLocation = "/simple_weather_icon_28.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_LIGHT_SNOW_SHOWER_DAY)){
+            fileLocation = "/simple_weather_icon_16.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_CLOUDY_LIGHT_SNOW)){
+            fileLocation = "/simple_weather_icon_24.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_HEAVY_SHOW_SHOWER_DAY)){
+            fileLocation = "/simple_weather_icon_12.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_CLOUDY_WITH_HEAVY_SNOW)){
+            fileLocation = "/simple_weather_icon_25.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_THUNDERY_SHOWER_NIGHT)){
+            fileLocation = "/simple_weather_icon_37.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_THUNDERY_SHOWER_DAY)){
+            fileLocation = "/simple_weather_icon_17.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_THUNDERSTORMS)){
+            fileLocation = "/simple_weather_icon_27.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_CLEAR_SKY_NIGHT)){
+            fileLocation = "/simple_weather_icon_02.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_SANDSTORM)){
+            fileLocation = "/simple_weather_icon_01.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_MIST_DAY)){
+            fileLocation = "/simple_weather_icon_01.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_FOG_DAY)){
+            fileLocation = "/simple_weather_icon_10.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_FOG_NIGHT)){
+            fileLocation = "/simple_weather_icon_10.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_TROPICAL_STORM)){
+            fileLocation = "/simple_weather_icon_30.png";
+        }
+        if(comparator.contains(WeatherConditions.WEATHER_HAZY)){
+            fileLocation = "/simple_weather_icon_10.png";
+        }
+        appWeatherIcon.setIcon(fileLocation);
+     
+        
+        
+        
+        
+    }
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private JButton appForecastButton;
     private JScrollPane appOutputTextArea;
