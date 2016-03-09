a3188ea50a8ea676fc614a75aa69a77aa67a69f8
James <eeu48c@bangor.ac.uk>
2016-03-09 15:27:27 +0000

    Added more weather conditions

--- src/main/java/net/spacejunkjim/miniproject3/WeatherConditions.java
+++ src/main/java/net/spacejunkjim/miniproject3/WeatherConditions.java
@@ -10,6 +10,43 @@ package net.spacejunkjim.miniproject3;
  * @author jamesashford
  */
 public class WeatherConditions {
+
+    /*
+    public static final String WEATHER_SUNNY_DAY = "Sunny Day";
+    public static final String WEATHER_PARTLY_CLOUDY = "Partly cloudy (night)";
+    public static final String WEATHER_SUNNY_INTERVALS = "Sunny Intervals";
+    public static final String WEATHER_MEDIUM_COLOUD = "White Medium Level Cloud";
+    8:   black low level cloud
+    9:   light rain shower (night)
+    10: light rain shower (day)
+    11: drizzle
+    12: light rain day or night
+    13: heavy rain shower (night)
+    14: heavy rain shower (day)
+    15: heavy rain
+    16: sleet shower (night)
+    17: sleet shower (day)
+    18: cloudy with sleet
+    19: hail shower (night)
+    20: hail shower (day)
+    21: cloudy with hail
+    22: light snow shower (night)
+    23: light snow shower (day)
+    24: cloudy with light snow
+    25: heavy snow shower (night)
+    26: heavy snow shower (day)
+    27: cloudy with heavy snow
+    28: thundery shower (night)
+    29: thundery shower (day)
+    30: thunderstorms
+    The remainder have been represented variously by plain text or images. They are :
+    0: clear sky (night)
+    4: sandstorm
+    5: mist day / night
+    6: fog day / night
+    31: tropical storm
+    32: hazy
+     */
     public static final String WEATHER_SUNNY = "Sunny";
     public static final String WEATHER_CLOUDY = "Cloudy";
     public static final String WEATHER_LIGHT_CLOUDS = "Light Clouds";
@@ -19,4 +56,3 @@ public class WeatherConditions {
     public static final String WEATHER_SNOW = "Snow";
     public static final String WEATHER_LIGHTNING = "Lightning";
 }
-
